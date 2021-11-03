76c9c8c840b26b6973bad37de9ee09173c0a1635
LeonXZhou <leon.zhou@queensu.ca>
2021-11-03 00:48:39 +0000

    implemented typerwriter.js

---
+++ typewriter.js
@@ -0,0 +1,7 @@
+const sentence = "hello there from lighthouse labs";
+let interval = 0;
+for (const char of sentence) {
+  setTimeout(()=>{process.stdout.write(char)},interval);
+  interval = interval +50;
+}
+setTimeout(()=>{process.stdout.write('\n')},interval)
